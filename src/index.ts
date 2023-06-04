e842d0c07842b935c98828227c8300e9f1730288
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-06-04 17:47:19 +0200

    import from node:buffer instead

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Buffer } from "buffer";
+import { Buffer } from "node:buffer";
 
 const buf = Buffer.from("006e0061006e00640065007200000000000000000000", "hex");
 const decodedName = buf.swap16().toString("utf16le");
